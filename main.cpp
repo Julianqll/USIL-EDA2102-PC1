5bfcf5897b3e488470c992de0fb72c4d41adf753
danielgancino <70718629+danielgancino@users.noreply.github.com>
2021-09-13 17:05:34 -0500

    PREGUNTA 12

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace :: std;
+
+struct nodo{
+
+  int dato;
+  nodo *sig;
+}*inicio,*fin;
+
+void insertarNodo();
+
+int main() {
+
+
+  return 0;
+}
+
+
+
+
+
+
+
+
+
+
