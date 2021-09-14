f47eeea1e38fb0a3081aa8975adbecc61ed1a1c1
Miguel Angel Ucañay Quesquén <63082198+miguel16974@users.noreply.github.com>
2021-09-13 20:48:46 -0500

    Solución pregunta-15

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include "linkedlist.h"
+#include "type.h"
+#include "stack.h"
+using namespace std;
+int main()
+{
+  TS cadena="miguel";
+  TS aux_cadena = "";
+  Stack<TX> stack;
+  Stack<TX> *pStack = new Stack<TX>;
+  for(int i=cadena.size();i>0;i--)
+  aux_cadena += cadena[i];
+  stack.push(cadena+cadena);
+  cout <<cadena<<endl;
+    /*string cadena = "miguel";
+    string aux_cadena = "";
+ 
+    for (int i = cadena.size(); i >=0 ; i--)
+         aux_cadena += cadena[i];
+
+
+    cout<<"El texto normal es: "<<cadena<<endl;
+    cout<<"El texto invertido es: "<<aux_cadena<<endl;
+    return 0;*/
+}
