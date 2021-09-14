74dd6eed472c0a626d7801047c546a23b60fe183
Miguel Angel Ucañay Quesquén <63082198+miguel16974@users.noreply.github.com>
2021-09-13 21:56:21 -0500

    Pregunta 12

--- main.cpp
+++ main.cpp
@@ -7,8 +7,13 @@ const int nElem=5;
 int main() {
   TX vect[nElem]={1,2,3,4,5};
   LinkedList<TX> mylist;
-  for(auto x=0; x<10; x++)
+  for(auto x=0; x<nElem; x++)
   {   
     mylist.insert_at_tail(vect[x]);
+    mylist.print(cout);
+    cout<< "" <<endl;
   }
+
+ 
+ 
 }
