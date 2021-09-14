5ee4ecf9286d1f23ffacbadf9545589bbdedc356
EstefanoCarbajal <82296903+EstefanoCarbajal@users.noreply.github.com>
2021-09-13 21:34:14 -0500

    Solucion 14

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "type.h"
+#include "stack.h"
+#include "queue.h"
+#include "linkedlist.h"
+
+
+using namespace std;
+
+const int nElem = 10;
+
+int main()
+{
+  
+    auto x=10;
+
+    Stack<T1> Stack;
+    
+        Stack.push(x);
+        
+
+        Stack.pop();
+    
+    cout<<numeroOctal;
+}
