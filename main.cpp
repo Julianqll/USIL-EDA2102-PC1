cc9c1bec290989134a8aa5e455083d05bc894c09
Miguel Angel Ucañay Quesquén <63082198+miguel16974@users.noreply.github.com>
2021-09-13 21:10:03 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,15 +4,18 @@
 #include "stack.h"
 using namespace std;
 int main()
+
 {
   TS cadena="miguel";
   TS aux_cadena = "";
   Stack<TX> stack;
   Stack<TX> *pStack = new Stack<TX>;
+  TX i=0;
+
   for(int i=cadena.size();i>0;i--)
   aux_cadena += cadena[i];
-  stack.push(cadena+cadena);
-  cout <<cadena<<endl;
+  stack.push(i+i);
+  cout <<aux_cadena<<endl;
     /*string cadena = "miguel";
     string aux_cadena = "";
  
