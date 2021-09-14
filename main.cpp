0b7083197a1c0cc78dc49b89bc1551b1b90f149b
Miguel Angel Ucañay Quesquén <63082198+miguel16974@users.noreply.github.com>
2021-09-13 20:22:43 -0500

    Solución pregunta-12

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "linkedlist.h"
+#include "type.h"
+
+using namespace std;
+const int nElem=5;
+int main() {
+  TX vect[nElem]={1,2,3,4,5};
+  LinkedList<TX> mylist;
+  for(auto x=0; x<10; x++)
+  {   
+    mylist.insert_at_tail(vect[x]);
+  }
+}
